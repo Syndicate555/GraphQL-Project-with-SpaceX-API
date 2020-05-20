72e9dcc505caf32fd0b01bc8fa4d6295d98d506c
Syndicate555 <saziz028@uottawa.ca>
2020-05-20 01:28:20 -0400

    Update schema.js

--- schema.js
+++ schema.js
@@ -6,9 +6,9 @@ const LaunchType = new GraphQLObjectType({
   name: "Launch",
   fields: () => ({
     flight_number: { type: GraphQLInt },
-    mission_name: { type: GraphQLInt },
-    flight_number: { type: GraphQLInt },
-    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
     flight_number: { type: GraphQLInt },
     flight_number: { type: GraphQLInt },
   }),
