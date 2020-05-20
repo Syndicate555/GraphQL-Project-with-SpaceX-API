4ebe76c1a6035415ffea48460e727dc570b3c22f
Syndicate555 <saziz028@uottawa.ca>
2020-05-20 19:10:07 -0400

    Update schema.js

--- schema.js
+++ schema.js
@@ -58,23 +58,23 @@ const RootQuery = new GraphQLObjectType({
           .then((res) => res.data);
       },
     },
-    launches: {
-      type: new GraphQLList(LaunchType),
+    rockets: {
+      type: new GraphQLList(RocketType),
       resolve(parent, args) {
         return axios
-          .get("https://api.spacexdata.com/v3/launches")
+          .get("https://api.spacexdata.com/v3/rockets")
           .then((res) => res.data);
       },
     },
 
-    launch: {
-      type: LaunchType,
+    rocket: {
+      type: RocketType,
       args: {
-        flight_number: { type: GraphQLInt },
+        id: { type: GraphQLInt },
       },
       resolve(parent, args) {
         return axios
-          .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+          .get(`https://api.spacexdata.com/v3/rockets/${args.id}`)
           .then((res) => res.data);
       },
     },
