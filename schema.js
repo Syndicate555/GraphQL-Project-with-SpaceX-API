51a1ae66c49e073e0348d67154f6ea01a89d3289
Syndicate555 <saziz028@uottawa.ca>
2020-05-20 01:25:30 -0400

    Update schema.js

--- schema.js
+++ schema.js
@@ -1,8 +1,15 @@
-const { GraphQLObjectType } = require("graphql");
+const { GraphQLObjectType, GraphQLInt } = require("graphql");
 
 // Launch Type
 
 const LaunchType = new GraphQLObjectType({
   name: "Launch",
-  fields: () => ({}),
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    flight_number: { type: GraphQLInt },
+    flight_number: { type: GraphQLInt },
+    flight_number: { type: GraphQLInt },
+    flight_number: { type: GraphQLInt },
+    flight_number: { type: GraphQLInt },
+  }),
 });
