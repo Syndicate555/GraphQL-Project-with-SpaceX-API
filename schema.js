7e5ad1f679bd5a2346144873d382839afeeb6aa0
Syndicate555 <saziz028@uottawa.ca>
2020-05-20 01:43:37 -0400

    Update schema.js

--- schema.js
+++ schema.js
@@ -20,3 +20,15 @@ const LaunchType = new GraphQLObjectType({
 });
 
 // Rocket Type
+
+const RocketType = new GraphQLObjectType({
+  name: "Launch",
+  fields: () => ({
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    rocket: { type: RocketType },
+  }),
+});
