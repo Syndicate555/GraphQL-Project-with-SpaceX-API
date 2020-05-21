06c7d422b9d7333a686c0ddb73b63c9fd0ea592e
Syndicate555 <saziz028@uottawa.ca>
2020-05-21 01:46:01 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>SpaceX</h1>
+    </div>
+  );
 }
 
 export default App;
